e51d89624cdb60a902eb588c7d612122849b37e6
DESKTOP-5IGA7VJ\bnp21 <seohee1590@gmail.com>
2022-05-17 17:04:42 +0900

    2022.5.17 디자인베이스 클론코딩
    반응형css 및 jquery기능 추가

--- design-base/assets/js/common.js
+++ design-base/assets/js/common.js
@@ -1,12 +1,14 @@
 $(function(){
     headerEffect();
+    activeBtn();
+    mobNav();
 });
 
 
 function headerEffect(){
     $(document).ready(function(){
         $(window).scroll(function(){
-            if($(window).scrollTop() > 1){
+            if($(window).scrollTop() > 10){
                 $('header').addClass('scroll');
             }else{
                 $('header').removeClass('scroll');
@@ -24,3 +26,38 @@ function headerEffect(){
         });
     });
 }
+
+function activeBtn(){
+    var navList = $('.gnb > li');
+    $(navList).click(function(){
+        $(this).addClass('is-active');
+        $(this).siblings().removeClass('is-active');
+    });
+}
+
+function mobNav(){
+    //mob서브메뉴
+    var moSubNav = $('.mob-nav > .gnb > li.has-sub');
+    $('.mob-submenu-btn').click(function(){
+        $(this).toggleClass('rotate');
+        $(moSubNav).find('.submenu').toggleClass('is-active');
+    });
+
+    //햄버거 아이콘
+    $('.hamburger').click(function() {
+        $(this).toggleClass('is-opened');
+        $('.mob-bg .mob-nav').toggleClass('is-active');
+        $('.mob-bg').toggleClass('is-active');
+    });
+    
+    //모바일->pc 사이트맵 숨기기
+    $( window ).resize(function() {
+    	var windowWidth = $( window ).width();
+    	if(windowWidth > 855) {
+    		$('.mob-bg').removeClass('is-active');
+            $('.mob-bg .mob-nav').removeClass('is-active');
+    	}
+     });
+}
+
+
